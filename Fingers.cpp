ec2ef86efa366b81d25b1fe53f5966bc8b5bc441
Omkar Ekbote <omkar.bugfix@live.com>
2013-03-30 18:12:35 +0530

    Formalized Fingers module
    Albeit after integration!

--- Fingers.cpp
+++ Fingers.cpp
@@ -1,38 +1,56 @@
-#include<iostream.h>
-#include<conio.h>
-#include<graphics.h>
-#include<dos.h>
-#include<time.h>
-#include<stdlib.h>
+/*
+                  NAME - MyButami™ Fingers Module
+               AUTHORS - Omkar Ekbote & Swaroop Bhonde
+           DESCRIPTION - Graphical representations of Fingers
+		COMPAITIBILITY - Borland Turbo C++ IDE (Compilation)
+						 DOS or similar Environment Shell
+			 COPYRIGHT - *Not decided by the Authors*
+			   VERSION - v0.9.2 BETA (Modular)
+			 DATE/TIME -28/03/07 18:44
+
+*/
+
+#include "Header.h"                                 //Custom MyButami™ Source© Header v1.0
 
 void main()
-	{
-	 clrscr();
-
+{
+ /*Auto-Detect & Initialize Graphics Drivers*/
 	 int gdriver = DETECT, gmode;
-	 initgraph(&gdriver, &gmode, "");
-	 for(int i = 0;i<=200; i+=45)
-		{
+	 initgraph(&gdriver,&gmode,"");
 
-		 line(50+i,50,50+i,120);
-		 line(90+i,50,90+i,120);
-		 arc(70+i,50,0,180,20);
-		 arc(70+i,50,0,180,15);
-		 arc(70+i,65,210,330,15);
-		 line(55+i,50,57+i,73);
-		 line(85+i,50,83+i,73);
-		}
-	  for(i = 250;i<=450; i+=45)
-		{
+ int orX=40,orY=80;
+ line(orX,orY,orX,orY+60); 		//Pl1hL
+ line(orX+30,orY,orX+30,orY+75);
+ line(orX,orY+60,orX+30,orY+85);
+ line(orX+10,orY+30,orX+20,orY+30);
+ line(orX+10,orY+30,orX+10,orY+60);
+ line(orX+10,orY+60,orX+30,orY+75);
 
-		 line(50+i,50,50+i,120);
-		 line(90+i,50,90+i,120);
-		 arc(70+i,50,0,180,20);
-		 arc(70+i,50,0,180,15);
-		 arc(70+i,65,210,330,15);
-		 line(55+i,50,57+i,73);
-		 line(85+i,50,83+i,73);
-		}
-	 getch();
-	 closegraph();
-	}
+ orX = 100;
+ line(orX,orY,orX,orY+75);		//Pl1hR
+ line(orX+30,orY,orX+30,orY+60);
+ line(orX+20,orY+30,orX+20,orY+60);
+ line(orX+20,orY+60,orX,orY+75);
+ line(orX+10,orY+30,orX+20,orY+30);
+ line(orX+30,orY+60,orX,orY+85);
+
+ orX=40;
+ orY=200;
+ line(orX+30,orY+10,orX+30,orY+85);	//Pl2hL
+ line(orX,orY+25,orX,orY+85);
+ line(orX,orY+25,orX+30,orY);
+ line(orX+10,orY+25,orX+30,orY+10);
+ line(orX+10,orY+25,orX+10,orY+55);
+ line(orX+10,orY+55,orX+20,orY+55);
+
+ orX=100;
+ line(orX,orY+10,orX,orY+85);      //Pl2hR
+ line(orX+30,orY+25,orX+30,orY+85);
+ line(orX,orY,orX+30,orY+25);
+ line(orX,orY+10,orX+20,orY+25);
+ line(orX+20,orY+25,orX+20,orY+55);
+ line(orX+10,orY+55,orX+20,orY+55);
+
+ getch();
+ closegraph();
+}
