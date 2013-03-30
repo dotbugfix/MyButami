752843d230eb98c7f407ee62ed1eba5a4d30eab4
Omkar Ekbote <omkar.bugfix@live.com>
2013-03-30 18:11:02 +0530

    GUI Improvements

--- MyButami.cpp
+++ MyButami.cpp
@@ -1,14 +1,16 @@
 /*
                   NAME - MyButami™ GUI Module
                AUTHORS - Omkar Ekbote & Swaroop Bhonde
-           DESCRIPTION - Simple Butami Game            <--- EDIT Required
+           DESCRIPTION - GUI Development Module
              COPYRIGHT - *Not decided by the Authors*
-               VERSION - v0.9.0 BETA Development
+               VERSION - v0.9.2 BETA Development
                 MODULE - MyButami™ Graphics (GUI)
-             DATE/TIME - 11/02/07 19:31
-VERSION SPECIFICATIONS - Individual GUI Module
+             DATE/TIME - 28/03/07 18:46
+VERSION SPECIFICATIONS - GUI Framework
+                         Drawing all 8+8 Fingers
+                         Generalizing for Source©
             KNOWN BUGS - Under Active Development!
-        NEXT BIG THING - Clock Display (Top Right Corner)
+		NEXT BIG THING - N/A
 
 */
 
@@ -16,6 +18,10 @@ VERSION SPECIFICATIONS - Individual GUI Module
 #include<conio.h>
 #include<graphics.h>
 
+unsigned short int Pl1hL = 4, Pl1hR = 4, Pl2hL = 4, Pl2hR = 4;
+
+int orX, orY;
+void DrawFingers();
 void main()
 	{
 /*Auto-Detect & Initialize Graphics Drivers*/
@@ -51,7 +57,32 @@ void main()
 	 outtextxy(368,49,"v0.9.0 BETA");        //LogoBottomRight Version
 	 settextstyle(4,0,7);
 	 outtextxy(180,-15,"MyButami");          //Logo
-	 setlinestyle(3,1,1);
+	// setlinestyle(3,1,1);
+											 //Fingers Module Begins
+/*Draw the UPPER_LEFT Finger*/
+ /*int orX=30,orY=110;
+ for(int count=4 ; count>0 ; orX+=40,count--)
+ {
+  line(orX,orY,orX,orY+60);
+  line(orX+30,orY,orX+30,orY+85);
+  line(orX,orY+60,orX+30,orY+85);
+  line(orX+10,orY+30,orX+10,orY+60);
+  line(orX+10,orY+60,orX+30,orY+75);
+ }
+ orX+=80;
+ for(count=4 ;
+ count>0 ; orX+=40,count--)
+ {
+  line(orX,orY,orX,orY+60);
+  line(orX+30,orY,orX+30,orY+85);
+  line(orX,orY+60,orX+30,orY+85);
+  line(orX+10,orY+30,orX+10,orY+60);
+  line(orX+10,orY+60,orX+30,orY+75);
+ }
+*/
+/*Draw fingers*/
+DrawFingers();
+											 //Fingers Module Ends
 /*Create ActvWindows for Console I/O*/
 	 window(58,8,77,25);
 	 cout<<"Standard Functions";
@@ -60,4 +91,29 @@ void main()
 	 getch();
 /*Close Graphics Driver*/
 	 closegraph();
-    }
+	}
+void DrawFingers()
+{
+ setviewport(4,96,442,getmaxy()-71,0);
+ /*Pl1hL*/
+ for(int Count = 1, orX = 171, orY = 25; Count<=Pl1hL; Count++, orX-=45)
+ {
+  line(orX,orY,orX,orY+60);
+  line(orX+30,orY,orX+30,orY+85);
+  line(orX,orY+60,orX+30,orY+85);
+  line(orX+10,orY+30,orX+10,orY+60);
+  line(orX+10,orY+60,orX+30,orY+75);
+ }
+ /*Pl1hR*/
+ for(Count = 1, orX= 221, orY=25; Count<=Pl1hR; Count++, orX+=45)
+ {
+  line(orX,orY,orX,orY+75);
+  line(orX+30,orY,orX+30,orY+60);
+  line(orX+20,orY+30,orX+20,orY+60);
+  line(orX+20,orY+60,orX,orY+75);
+  line(orX+10,orY+30,orX+20,orY+30);
+  line(orX+30,orY+60,orX,orY+85);
+ }
+
+
+}
